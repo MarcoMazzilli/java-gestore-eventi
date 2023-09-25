9039aa0fc5d48dbee200dad64c3d2f28d65f70ab
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-25 14:05:09 +0200

    [Add] Milestone 2

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -2,6 +2,7 @@ package org.java;
 
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import org.java.evento.Evento;
 
@@ -9,42 +10,84 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		LocalDate eventDate = LocalDate.parse("2025-11-23");
-		
+		Scanner sc = new Scanner(System.in);
 		Evento ev1 = new Evento();
-		System.out.println(ev1 + "\n");
 		
-		// TRY-CATCH VALORIZZAZIONE EVENTO;
+		// CREAZINE NUOVO EVENTO
+			
+
+		System.out.println("Inserisci il nome dell'evento: ");
+		String nomeEvento = sc.nextLine();
+				
+		System.out.println("Inserisci la data nel seguente formato 'yyyy-mm-gg': ");
+		LocalDate dataEvento = LocalDate.parse(sc.nextLine());
+				
+		System.out.println("Inserisci il N. di posti totali: ");
+		int nPostiTotale = Integer.valueOf(sc.nextLine());
+				
+			try {
+				
+				ev1 = new Evento(nomeEvento, dataEvento, nPostiTotale);
+						
+			} catch (Exception e) {
+						
+				System.err.println(e.getMessage());	
+						
+			}
+				
+		System.out.println("\n" + ev1 + "\n");
 		
-		try {
-			 ev1 = new Evento("Spettacolo", eventDate , 0);
+		
+			
+		// AGGIUNTA PRENOTAZIONI EVENTO
+		
+		System.out.println("Quante prenotazioni vuoi aggiungere?");
+		int nPrenotazioniDaAggiungere = Integer.valueOf(sc.nextLine());
+				
+			for (int i = 1; i < nPrenotazioniDaAggiungere+1; i++) {
+					
+				try {
+						
+					ev1.prenota();
+						
+				} catch (Exception e) {
+						
+					System.err.println("La prenotazione n. " + i + " non e stata inserita, evento al completo\n");
+					System.err.println(e.getMessage());
+						
+					break;
+				}
+			}
+			
+			
+			// DISDETTA PRENOTAZIONI EVENTO
+				
+				
+			System.out.println("Quante prenotazioni vuoi disdire?");
+			int prenotazioniDaDisdire = Integer.valueOf(sc.nextLine());
+				
+				
+			for (int i = 0; i < prenotazioniDaDisdire; i++) {
+					
+				try {
+					ev1.disdici();
+					
+				} catch (Exception e) {
+						
+					System.err.println(e.getMessage());
+					break;
+						
+				}
+					
+			}
+				
 			System.out.println(ev1);
-			
-		} catch (Exception e) {
-			
-			System.err.println(e.getMessage());
+			sc.close();
+				
+				
 		}
-		
-		//TRY-CATCH PRENOTA
-		
-		try {
-			ev1.prenota();
-
-
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		
-		// TRY-CATCH DISDICI
-		
-//		try {
-//			ev1.disdici();
-//			
-//		} catch (Exception e) {
-//			System.err.println(e.getMessage());
-//		}
-		System.out.println("\n--------------\n");
-		System.out.println("updated: " + ev1);
-	}
-
 }
+		
+		
+
+	
